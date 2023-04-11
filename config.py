81e55f17ac5cea22a8d7a21e7e53a0c3423678aa
qingfeng35 <130447678+qingfeng35@users.noreply.github.com>
2023-04-11 16:44:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cpgxyl71ntdy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "taykhinfung-employee"
+customregion = "us-east-1"
